e816e0f866180bea63c7a20c7f90d14047f39a5e
zfersya <zfersya@gmail.com>
2025-05-28 01:07:18 +0700

    contoh

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -15,6 +15,14 @@ public class Driver {
 		counter.decrement();
 		
 		System.out.println("Current count:" + counter.getCount() );
+
+			counter.decrement();
+		
+		System.out.println("Current count:" + counter.getCount() );
+
+			counter.decrement();
+		
+		System.out.println("Current count:" + counter.getCount() );
 		
 	}
 
